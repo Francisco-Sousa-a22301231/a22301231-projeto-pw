47fde5fdd0e53011251d53efe718027544ed596c
Francisco-Sousa-a22301231 <125379138+Francisco-Sousa-a22301231@users.noreply.github.com>
2024-06-11 10:04:57 +0100

    Update forms.py

--- artigos/forms.py
+++ artigos/forms.py
@@ -1,5 +1,21 @@
 from django import forms
-from artigos.models import Comentario
+from django.contrib.auth.models import User
+from .models import Tema, Autor, Artigo, Comentario, Avaliacao
+
+class TemaForm(forms.ModelForm):
+    class Meta:
+        model = Tema
+        fields = ['titulo']
+
+class AutorForm(forms.ModelForm):
+    class Meta:
+        model = Autor
+        fields = ['usuario', 'foto', 'bio']
+
+class ArtigoForm(forms.ModelForm):
+    class Meta:
+        model = Artigo
+        fields = ['titulo', 'conteudo', 'autor', 'imagem', 'tema']
 
 class ComentarioForm(forms.ModelForm):
     class Meta:
@@ -8,3 +24,8 @@ class ComentarioForm(forms.ModelForm):
         widgets = {
             'conteudo': forms.Textarea(attrs={'rows': 4}),
         }
+
+class AvaliacaoForm(forms.ModelForm):
+    class Meta:
+        model = Avaliacao
+        fields = ['artigo', 'autor', 'pontuacao', 'comentario']
