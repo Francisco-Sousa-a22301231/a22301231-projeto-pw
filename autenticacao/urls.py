59d4223ca1d4e8bf3e37cf28f10491968ca6ccd0
Francisco-Sousa-a22301231 <125379138+Francisco-Sousa-a22301231@users.noreply.github.com>
2024-06-11 02:31:20 +0100

    Update urls.py

--- autenticacao/urls.py
+++ autenticacao/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LogoutView
+from allauth.account.views import logout
 
 app_name = 'autenticacao'
 
@@ -8,7 +10,8 @@ urlpatterns = [
     path("register/", views.register, name="register"),
     path("profile/", views.profile, name="profile"),
     path("", views.loginPage, name="login"),
-    path('logout/', auth_views.LogoutView.as_view(next_page='/autenticacao/'), name='logout'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('accounts/logout/', logout, name='account_logout'),
     path('update_profile/', views.update_profile, name='update_profile'),
 
 ]
